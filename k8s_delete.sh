986be376860491a13a5098dfd4153873c9669ea6
Roland VM <dasycarpum@gmail.com>
2023-09-03 10:09:18 +0200

    Improve pod deletion script with NAMESPACE argument

--- k8s_delete.sh
+++ k8s_delete.sh
@@ -6,8 +6,14 @@ declare -a deployments=("postgres-db-deployment" "python-app" "firefox-deploymen
 # Declare an array of services to delete
 declare -a services=("postgres-service" "python-app-service" "firefox-service")
 
-# Namespace in which to delete the resources
-NAMESPACE="nyt-data"
+# Namespace in which to delete the resources (accepts as a command-line argument)
+NAMESPACE="$1"
+
+# Validation to ensure NAMESPACE is not empty
+if [ -z "$NAMESPACE" ]; then
+  echo "Please provide a NAMESPACE as an argument."
+  exit 1
+fi
 
 # Loop to delete deployments
 for deployment in "${deployments[@]}"
@@ -27,4 +33,4 @@ kubectl delete ingress my-app-ingress -n ${NAMESPACE}
 
 # kubectl delete pvc postgres-pvc -n ${NAMESPACE}
 
-echo "All specified deployments and services have been deleted."
+echo "All specified deployments and services have been deleted in NAMESPACE ${NAMESPACE}." 
