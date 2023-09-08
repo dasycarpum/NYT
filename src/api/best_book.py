a7a84f6111729ca9afc98313d43480975be62996
Roland VM <dasycarpum@gmail.com>
2023-09-08 09:16:36 +0200

    add files

---
+++ src/api/best_book.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2023-09-08
+
+@author: Roland
+
+@abstract: by setting the book genre and year of publication, the API will return the titles and authors of the top 5 bestsellers, based on the number of weeks remaining in the ranking.
+"""
+
+
---
+++ src/api/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2023-09-08
+
+@author: Roland
+
+@abstract: The aim of this part of the application is to use the "nyt" database to allow the user to query it via an API. The results will be returned in the form of a JSON file.
+"""
+
